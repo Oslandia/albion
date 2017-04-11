3f721ca8559fa86be06c31176ca89f45ac99b6af
Damien Garaud <damien.garaud@oslandia.com>
2017-04-11 16:38:19 +0200

    Clean up the package module
    * fix the uninstall function
    * remove the unused (and possibly broken) update_meta and deploy functions

--- package.py
+++ package.py
@@ -3,7 +3,7 @@
 packaging script for the albion project
 
 USAGE
-    package.py [-h, -i, -u] [directory],
+    package.py [-h, -i, -u, -t] [directory],
 
 OPTIONS
     -h, --help
@@ -15,9 +15,6 @@ OPTIONS
 
     -u, --uninstall
         uninstall (remove) the package from .qgis2 directory
-
-    -d, --deploy
-        deploy the package to qgis repository directory
 
     -t
         launch the tests before installing/uninstalling
@@ -33,9 +30,9 @@ import subprocess
 
 # @todo make that work on windows
 qgis_plugin_dir = os.path.join(os.path.expanduser("~"), ".qgis2", "python", "plugins")
-repository_dir = "//Dev/repository/"
 zipname = "albion"
 zipext = ".zip"
+
 
 def run_tests():
     out, err = subprocess.Popen(["pytest"],
@@ -46,36 +43,13 @@ def run_tests():
         print out
         exit(1)
 
-def deploy(zip_filename, new_version):
-    file_name = repository_dir + zipname + "_" + str(new_version) + zipext
-    shutil.copyfile(zip_filename,file_name)
-    xmlfile = os.path.join(repository_dir, "albion.xml")
-    with open(xmlfile, 'r') as file:
-        data = file.readlines()
-    data[3]= "  <pyqgis_plugin name='albion' version='" + str(new_version) + "'>\n"
-    data[5]= "    <version>" + str(new_version) + "</version>\n"
-    data[10]= "    <download_url>file://" + file_name +  "</download_url>\n"
-    data[11]= "    <uploaded_by>" + getpass.getuser() + "</uploaded_by>\n"
-    data[12]= "    <create_date>" + time.strftime("%x") + "</create_date>\n"
-    with open(xmlfile, 'w') as file:
-        file.writelines( data )
-
-def update_meta():
-    base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    meta_dir = os.path.join(base_dir, "metadata.txt")
-    with open(meta_dir, 'r') as file:
-        data = file.readlines()
-    newversion = float(data[12].split('=')[1]) + 0.01
-    data[12]= "version="+str(newversion)+"\n"
-    print data[12]
-    with open(meta_dir, 'w') as file:
-        file.writelines( data )
-    return newversion
-
 def uninstall(install_dir):
-    target_dir = os.path.join(install_dir, "base_dir")
+    target_dir = os.path.join(install_dir, "albion")
     if os.path.isdir(target_dir):
+        print "uninstall dir", target_dir
         shutil.rmtree(target_dir)
+    else:
+        print "Install directory '%s' not found" % target_dir
 
 def install(install_dir, zip_filename):
     uninstall(install_dir)
@@ -88,7 +62,7 @@ def zip_(zip_filename):
     base_dir = os.path.abspath(os.path.dirname(__file__))
     with zipfile.ZipFile(zip_filename, 'w') as package:
         for root, dirs, files in os.walk(base_dir):
-            if not re.match(r".*(test_data|docs).*", root):
+            if not re.match(r".*(test_data|docs|tests).*", root):
                 for file_ in files:
                     if re.match(r".*\.(w15|py|txt|ui|json|sql|png|svg|qml)$", file_) \
                             and not re.match(r".*(_test|_demo)\.py", file_) \
@@ -119,10 +93,6 @@ if __name__ == "__main__":
     if "-t" in optlist:
         run_tests()
 
-    if "-d" in optlist or "--deploy" in optlist:
-        run_tests()
-        new_version = update_meta()
-
     zip_filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), zipname+zipext)
     zip_(zip_filename)
     install_dir = qgis_plugin_dir if len(args)==0 else args[0]
@@ -132,7 +102,4 @@ if __name__ == "__main__":
 
     if "-i" in optlist or "--install" in optlist:
         install(install_dir, zip_filename)
-
-    if "-d" in optlist or "--deploy" in optlist:
-        deploy(zip_filename, new_version)
 
