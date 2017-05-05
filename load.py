3e2c868a1e569b185aa0d69351cb2ffceff89da8
Vincent Mora <vincent.mora@oslandia.com>
2017-05-05 16:19:16 +0200

    x3 speedup for loading

--- load.py
+++ load.py
@@ -24,79 +24,16 @@ def load_collar(cur, filename, progress):
             """, data)
 
 def load_devia(cur, filename, progress=None):
-    with open(filename) as f:
-        header = f.readline()
-        #in_ = StringIO.StringIO("\n".join(["{}\t{}\t{}\t{}".format(d[0], d[1], d[2], d[3]) for d in data]))
-        cur.copy_from(StringIO.StringIO(f.read().replace(';','\t')), '_albion.deviation')
-
-
-        # executemany doesn't do mush in psycopg2 (not true in sqlite), so we chunk to perform better
-        #chunk_size = 1000
-        #i = 0
-        #while i < len(data):
-        #    chunk = ','.join([cur.mogrify('(%s,%s,%s,%s)', row) for row in data[i:min(i+chunk_size, len(data))]])
-        #    cur.execute("""
-        #        insert into albion.deviation(hole_id, from_, deep, azimuth) 
-        #        values 
-        #        """ + chunk)
-        #    if progress:
-        #        progress.set_ratio(float(i)/len(data))
-
-        #    i += chunk_size
-
-        #cur.executemany("""
-        #    insert into albion.deviation(hole_id, from_, deep, azimuth) 
-        #    values (%s, %s, %s, %s)
-        #    """, data)
+    cur.copy_expert("copy _albion.deviation from stdin delimiter ';' csv header", open(filename))
 
 def load_formation(cur, filename, progress):
-    with open(filename) as f:
-        header = f.readline()
-        convert = lambda x: (x[0], float(x[1]),  float(x[2]), int(x[3]), x[4])
-        data = [convert(line.rstrip().split(';')) for line in f]
-        cur.executemany("""
-            insert into albion.formation(hole_id, from_, to_, code, comments) 
-            values (%s, %s, %s, %s, %s)
-            """, data)
+    cur.copy_expert("copy _albion.formation from stdin delimiter ';' csv header", open(filename))
 
 def load_resi(cur, filename, progress):
-    with open(filename) as f:
-        header = f.readline()
-        convert = lambda x: (x[0], float(x[1]),  float(x[2]), float(x[3]))
-        data = [convert(line.rstrip().split(';')) for line in f]
-
-        chunk_size = 1000
-        i = 0
-        while i < len(data):
-            chunk = ','.join([cur.mogrify('(%s,%s,%s,%s)', row) for row in data[i:min(i+chunk_size, len(data))]])
-            cur.execute("""
-                insert into albion.resistivity(hole_id, from_, to_, rho) 
-                values 
-                """ + chunk)
-            if progress:
-                progress.set_ratio(float(i)/len(data))
-
-            i += chunk_size
+    cur.copy_expert("copy _albion.resistivity from stdin delimiter ';' csv header", open(filename))
 
 def load_avp(cur, filename, progress):
-    with open(filename) as f:
-        header = f.readline()
-        convert = lambda x: (x[0], float(x[1]),  float(x[2]), float(x[3]))
-        data = [convert(line.rstrip().split(';')) for line in f]
-
-        chunk_size = 1000
-        i = 0
-        while i < len(data):
-            chunk = ','.join([cur.mogrify('(%s,%s,%s,%s)', row) for row in data[i:min(i+chunk_size, len(data))]])
-            cur.execute("""
-                insert into albion.radiometry(hole_id, from_, to_, gamma) 
-                values 
-                """ + chunk)
-            if progress:
-                progress.set_ratio(float(i)/len(data))
-
-            i += chunk_size
-
+    cur.copy_expert("copy _albion.radiometry from stdin delimiter ';' csv header", open(filename))
 
 
 def load_file(cur, filename, progress=None):
