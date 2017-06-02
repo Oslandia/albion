970c49189091d33d09ee38674bd084e55d2d98e5
Vincent Mora <vincent.mora@oslandia.com>
2017-06-02 19:47:29 +0200

    remove data from package

--- package.py
+++ package.py
@@ -60,7 +60,7 @@ def zip_(zip_filename):
     base_dir = os.path.abspath(os.path.dirname(__file__))
     with zipfile.ZipFile(zip_filename, 'w') as package:
         for root, dirs, files in os.walk(base_dir):
-            if not re.match(r".*(test_data|docs|tests).*", root):
+            if not re.match(r".*(test_data|docs|tests|niger_data).*", root):
                 for file_ in files:
                     if re.match(r".*\.(py|txt|ui|json|sql|png|svg|qml|qgs)$", file_) \
                             and not re.match(r".*(_test|_demo)\.py", file_) \
@@ -91,8 +91,9 @@ if __name__ == "__main__":
     if "-t" in optlist:
         run_tests()
 
-    zip_filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), zipname+zipext)
+    zip_filename = os.path.join(os.path.dirname(__file__), zipname+zipext)
     zip_(zip_filename)
+    print "created", zip_filename
     install_dir = qgis_plugin_dir if len(args)==0 else args[0]
 
     if "-u" in optlist or "--uninstall" in optlist:
