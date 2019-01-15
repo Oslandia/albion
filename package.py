b854c6b7d29da27d2e1bdb1794a2512f9d647fb2
Blottiere Paul <blottiere.paul@gmail.com>
2019-01-15 13:56:13 +0000

    Black is back

--- package.py
+++ package.py
@@ -35,13 +35,14 @@ zipext = ".zip"
 
 
 def run_tests():
-    out, err = subprocess.Popen(["pytest"],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE).communicate()
-    if '0 failed' not in out.splitlines()[-1]:
+    out, err = subprocess.Popen(
+        ["pytest"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ).communicate()
+    if "0 failed" not in out.splitlines()[-1]:
         print("Can not deploy if test fails.")
         print(out)
         exit(1)
+
 
 def uninstall(install_dir):
     target_dir = os.path.join(install_dir, "albion")
@@ -51,26 +52,34 @@ def uninstall(install_dir):
     else:
         print("Install directory '{}' not found".format(target_dir))
 
+
 def install(install_dir, zip_filename):
     uninstall(install_dir)
     with zipfile.ZipFile(zip_filename, "r") as z:
         z.extractall(install_dir)
     print("installed in {}".format(install_dir))
 
+
 def zip_(zip_filename):
     """the zip file doesn't include tests, demos or docs"""
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    with zipfile.ZipFile(zip_filename, 'w') as package:
+    with zipfile.ZipFile(zip_filename, "w") as package:
         for root, dirs, files in os.walk(base_dir):
             if not re.match(r".*(test_data|__pycache__).*", root):
                 for file_ in files:
-                    if re.match(r".*\.(py|txt|ui|json|sql|png|svg|qml|qgs|html|gif|inv|js|css|zip)$", file_) \
-                            and not re.match(r".*(_test|_demo)\.py", file_) \
-                            and not re.match(r".*albion.zip$", file_) \
-                            and not re.match(r"(package.py|test.py)", file_):
+                    if (
+                        re.match(
+                            r".*\.(py|txt|ui|json|sql|png|svg|qml|qgs|html|gif|inv|js|css|zip)$",
+                            file_,
+                        )
+                        and not re.match(r".*(_test|_demo)\.py", file_)
+                        and not re.match(r".*albion.zip$", file_)
+                        and not re.match(r"(package.py|test.py)", file_)
+                    ):
                         fake_root = root.replace(base_dir, "albion")
-                        package.write(os.path.join(root, file_),
-                                      os.path.join(fake_root, file_))
+                        package.write(
+                            os.path.join(root, file_), os.path.join(fake_root, file_)
+                        )
 
 
 if __name__ == "__main__":
@@ -78,11 +87,11 @@ if __name__ == "__main__":
     import sys
 
     try:
-        optlist, args = getopt.getopt(sys.argv[1:],
-                "hiudt",
-                ["help", "install", "uninstall", "test"])
+        optlist, args = getopt.getopt(
+            sys.argv[1:], "hiudt", ["help", "install", "uninstall", "test"]
+        )
     except Exception as e:
-        sys.stderr.write(str(e)+"\n")
+        sys.stderr.write(str(e) + "\n")
         exit(1)
 
     optlist = dict(optlist)
@@ -94,17 +103,15 @@ if __name__ == "__main__":
     if "-t" in optlist:
         run_tests()
 
-    
     build_doc()
 
-    zip_filename = os.path.join(os.path.dirname(__file__), zipname+zipext)
+    zip_filename = os.path.join(os.path.dirname(__file__), zipname + zipext)
     zip_(zip_filename)
     print("created {}".format(zip_filename))
-    install_dir = qgis_plugin_dir if len(args)==0 else args[0]
+    install_dir = qgis_plugin_dir if len(args) == 0 else args[0]
 
     if "-u" in optlist or "--uninstall" in optlist:
         uninstall(install_dir)
 
     if "-i" in optlist or "--install" in optlist:
         install(install_dir, zip_filename)
-
